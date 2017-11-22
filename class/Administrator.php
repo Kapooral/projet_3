45971ee50a6cb5aef55b13461e901e25d04d711a
Mbenguia Husseini <mbenguia.husseini@live.fr>
2017-11-22 13:58:18 +0100

    Création de la class Administrator et de son manager

---
+++ class/Administrator.php
@@ -0,0 +1,63 @@
+<?php
+
+class Administrator
+{
+	private $_id;
+	private $_login;
+	private $_password;
+
+	public function __construct(array $data)
+	{
+		$this->hydrate($data);
+	}
+
+	public function hydrate(array $data)
+	{
+		foreach($data as $key => $value)
+		{
+			$method = 'set' . ucfirst($key);
+
+			if(method_exists($this, $method))
+			{
+				$this->$method($value);
+			}
+		}
+	}
+
+	public function setId($id)
+	{
+		$id = (int)$id;
+
+		if($id > 0)
+		{
+			$this->_id = $id;
+		}
+	}
+
+	public function setLogin($login)
+	{
+		$login = (string)$login;
+
+		$this->_login = $login;
+	}
+
+	public function setPassword($password)
+	{
+		$this->_password = $password;
+	}
+
+	public function id()
+	{
+		return $this->_id;
+	}
+
+	public function login()
+	{
+		return $this->_login;
+	}
+
+	public function password()
+	{
+		return $this->_password;
+	}
+}
---
+++ model/AdministratorManager.php
@@ -0,0 +1,58 @@
+<?php 
+
+require_once('Manager.php');
+
+class AdministratorManager extends Manager
+{
+	private $_db;
+
+	public function __construct()
+	{
+		$this->_db = $this->dbConnect();
+	}
+
+	public function add($login, $password)
+	{
+		$req = $this->_db->prepare('INSERT INTO administrator (login, password) VALUES (:login, :password)');
+		$req->bindValue(':login', $login);
+		$req->bindValue(':password', password_hash($password, PASSWORD_DEFAULT));
+		$req->execute();
+	}
+
+	public function exists($info)
+	{
+		if(is_int($info))
+		{
+			return (bool) $this->_db->query('SELECT COUNT(*) FROM administrator WHERE id = ' . $info)->fetchColumn();
+		}
+		else
+		{
+			$req = $this->_db->prepare('SELECT COUNT(*) FROM administrator WHERE login = :login');
+			$req->execute([':login' => $info]);
+
+			return (bool) $req->fetchColumn();
+		}
+	}
+
+	public function get($info, $password)
+	{
+		$req = $this->_db->prepare('SELECT id, login, password FROM administrator WHERE login = :login');
+		$req->execute(array($info));
+		
+		$data = $req->fetch(PDO::FETCH_ASSOC);
+
+		if(password_verify($password, $data['password']))
+		{
+			return new Administrator($data);
+		}
+	}
+
+	public function update(Administrator $admin)
+	{
+		$req = $this->_db->prepare('UPDATE administrator SET login = :login, password = :password WHERE id = :id');
+		$req->bindValue(':login', $admin->login());
+		$req->bindValue(':password', password_hash($admin->password(), PASSWORD_DEFAULT));
+		$req->bindValue(':id', $admin->id());
+		$req->execute();
+	}
+}
